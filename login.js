f9c6593efb2b61ce142f701b0a8bd45d6de294f7
thanacho1 <thanacho@metrosystems.co.th>
2023-12-12 09:56:34 +0700

    first commit

---
+++ login.js
@@ -0,0 +1,42 @@
+var jwt = localStorage.getItem("jwt");
+if (jwt != null) {
+  window.location.href = './bi.html'
+}
+
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "https://billreport.auth.ap-southeast-1.amazoncognito.com");
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(JSON.stringify({
+    "username": username,
+    "password": password
+  }));
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      const objects = JSON.parse(this.responseText);
+      console.log(objects);
+      if (objects['status'] == 'ok') {
+        localStorage.setItem("jwt", objects['accessToken']);
+        Swal.fire({
+          text: objects['message'],
+          icon: 'success',
+          confirmButtonText: 'OK'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = './bi.html';
+          }
+        });
+      } else {
+        Swal.fire({
+          text: objects['message'],
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+      }
+    }
+  };
+  return false;
+}
